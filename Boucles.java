9556c22308e1f37b48f218fb3886b3965a33f250
Aicha Benjdir <aicha.benjdir@isimg.tn>
2025-03-07 22:27:34 +0100

    Boucles

---
+++ Boucles.java
@@ -0,0 +1,36 @@
+public class Boucles
+{
+ public static void main(String[] args)
+    {
+        // int i = 1; 
+
+        // do
+        // {
+        //      i++; 
+
+        //       if(i == 5)
+        //       continue;
+        //     System.out.println(i); 
+
+           
+           
+        // }
+        // while(i != 10 );
+
+
+
+        int i = 0; 
+
+        while(i != 10)
+        {
+           System.out.println(i); 
+           i++; 
+        }
+        for(i = 0 ; i !=10 ; i++)
+        {
+            System.out.println(i); 
+        }
+    }
+}
+
+    
