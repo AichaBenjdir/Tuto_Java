3ed1b22c8e4999cdde62c3dc69be5e2b8dd7aea7
Aicha Benjdir <aicha.benjdir@isimg.tn>
2025-03-07 21:18:54 +0100

    Operateurs

--- Operateurs.java
+++ Operateurs.java
@@ -4,8 +4,18 @@
 = Affectation
 + Concatination
 
-+ - *  / % 
++ - *  / %  ()
+== === != < <= > >=
++= -= *= /= %= ------> A = A + x 
+                       A += x
 
+                       A = A-x 
+                       A -= x 
+
+ A++ post-incrémentatin  A = (A + 1  )
+++A  pré-incrémentation 
+ A-- post-décreméntation
+ --A pré-décrémentation
 */
 
 
@@ -16,8 +26,37 @@ public class Operateurs
 
 // int age = 24;  
 // System.out.println("Tu as " + age +  " ans"); 
- int resultat = 5 % 2; 
- System.out.println(resultat); 
+//  int resultat = 5 % 2; 
+//  System.out.println(resultat); 
 
+
+
+
+// int number = 4; 
+// int result = number; 
+// System.out.println(result); 
+
+// boolean myvar = true; 
+// System.out.println(myvar);
+
+
+
+
+// int experience = 0; 
+// System.out.println(experience);
+
+//  experience = 10; 
+// System.out.println(experience);
+
+//  experience = 100; 
+// System.out.println(experience);
+
+
+
+int nb1 = 1;
+int nb2 = nb1++;  //nb2 = nb1 puis nb1 += 1
+int nb2 = ++nb1;  // nb1 += 1 puis nb2 = nb1
+System.out.println(nb1); 
+System.out.println(nb2);
     }
 }
