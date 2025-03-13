7d38386e7863805e26106789255cfce3a8b9d3d0
Aicha Benjdir <aicha.benjdir@isimg.tn>
2025-03-13 21:27:44 +0100

    Construction d'une classe et de ses attributs

--- Construction d'une classe/App.java
+++
@@ -1,8 +0,0 @@
-public class App 
-{
-    public static void main(Sring []args)
-    {
-        Cat miaou = new Cat(); 
-        Cat miaou2 = new Cat(); 
-    }
-}
---
+++ ConstructionClasse/App.java
@@ -0,0 +1,9 @@
+public class App 
+{
+    public static void main(Sring []args)
+    {
+           Cat c = new Cat("kit", 1);
+           Cat c2 = new Cat("Felix", 5);
+           System.out.println(c.mName);
+    }
+}
